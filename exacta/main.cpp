df068f56de870bce8e6001ff18efe06300864683
Cat <catalinajuarros@gmail.com>
2017-10-17 21:24:56 -0300

    Hice la función que dice si hay C en línea

--- exacta/main.cpp
+++ exacta/main.cpp
@@ -23,6 +23,7 @@ public:
     void addPiece(column col, int player);
     void removePiece(column col);
     Winner checkGame();
+    bool connectC(int i, int j, int player);
 
 private:
     int rows;
@@ -55,7 +56,8 @@ private:
 };
 
 void Game::addPiece(column col, int player) {
-    // hay que chequear primero
+    // Si la celda más baja desocupada es -1, quiere decir
+    // que la columna está llena y no es válido poner fichas ahí.
     assert(lowestFreeCell.at(col) > -1);
 
     int i = lowestFreeCell.at(col);
@@ -64,6 +66,8 @@ void Game::addPiece(column col, int player) {
 }
 
 void Game::removePiece(column col) {
+    // Si no hay fichas en la columna (es decir, la celda desocupada
+    // más baja es la última), no es válido sacar una ficha.
     assert(lowestFreeCell.at(col) < rows - 1);
 
     int i = lowestFreeCell.at(col) + 1;
@@ -84,10 +88,112 @@ vector<column> Game::possibleMoves() {
 
 Winner Game::checkGame() {
     // TODO
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            if (connectC(i, j, 1)) {
+                // PLAYER1 = nosotros. ¡Chequear que esté bien!
+                return PLAYER1;
+            } else if (connectC(i, j, -1)) {
+                return PLAYER2;
+            }
+        }
+    }
     return DRAW;
 }
 
+bool Game::connectC(int i, int j, int player) {
+    // Dice si hay C piezas en línea del jugador dado que incluyan a la posición (i, j).
+    // Quiero pedirles perdón a Dijkstra, a Turing, a John von Neumann
+    // y al Prof. Francisco Soulignac por estos 7 whiles separados
+    // y las row y col que se resetean.
 
+    int downwards = 0, leftwards = 0, rightwards = 0; // Cuentan las fichas en vertical y horizontal
+    int leftUpwards = 0, leftDownwards = 0, rightUpwards = 0, rightDownwards = 0; // Cuentan las fichas en diagonal
+    int row = i, col = j; // Recorren la matriz
+
+    while (row < rows && board.at(row).at(j) == player) {
+        // Cuenta las fichas de player mirando para abajo.
+        ++downwards;
+        ++row;
+    }
+
+    if (downwards >= c) {
+        // C en línea vertical.
+        return true;
+    }
+
+    row = i; // Vuelve a i para mirar las diagonales.
+
+    while (col >= 0 && board.at(i).at(col) == player) {
+        // Cuenta las fichas de player mirando para la izquierda.
+        ++leftwards;
+        --col;
+    }
+
+    col = j + 1; // Vuelve a j + 1 para mirar a la derecha.
+    // Es j + 1 en vez de j para no contar dos veces board[i,j]
+    // en caso de que esté ocupada por player.
+
+    while (col < columns && board.at(i).at(col) == player) {
+        // Cuenta las fichas de player mirando para la derecha.
+        ++rightwards;
+        ++col;
+    }
+
+    if (leftwards + rightwards >= c) {
+        // C en línea horizontal.
+        return true;
+    }
+
+    col = j; // Vuelve a j para mirar las diagonales.
+
+    while (row >= 0 && col >= 0 && board.at(row).at(col) == player) {
+        // Cuenta las fichas de player mirando en diagonal para arriba a la izquierda.
+        ++leftUpwards;
+        --row;
+        --col;
+    }
+
+    row = i + 1; // Vuelve a i + 1 para mirar en diagonal para abajo a la derecha.
+    col = j + 1;
+
+    while (row < rows && col < columns && board.at(row).at(col) == player) {
+        ++rightDownwards;
+        ++row;
+        ++col;
+    }
+
+    if (leftUpwards + rightDownwards >= c) {
+        // C en una diagonal.
+        return true;
+    }
+
+    row = i; // Vuelve a i para mirar la diagonal en el otro ángulo.
+    col = j;
+
+    while (row < rows && col >= 0 && board.at(row).at(col) == player) {
+        // Cuenta las fichas de player mirando en diagonal para abajo a la izquierda.
+        ++leftDownwards;
+        ++row;
+        --col;
+    }
+
+    row = i - 1; // Vuelve a i - 1 para mirar en diagonal para arriba a la derecha.
+    col = j + 1;
+
+    while (row >= 0 && col < columns && board.at(row).at(col) == player) {
+        ++rightUpwards;
+        --row;
+        ++col;
+    }
+
+    if (leftDownwards + rightDownwards >= c) {
+        // C en la otra diagonal.
+        return true;
+    }
+
+    return false; // No contó C en línea en ninguna dirección.
+}
 
 pair <score, column> Player::minimax(int placedPieces, int maximizingPlayer) {
     if (placedPieces == game.p) {
@@ -107,7 +213,7 @@ pair <score, column> Player::minimax(int placedPieces, int maximizingPlayer) {
         int bestMove = 0;
         for (column col : game.possibleMoves()) {
             game.addPiece(col,1);
-            pair <score, column> value= minimax(placedPieces +1, -1);
+            pair <score, column> value= minimax(placedPieces + 1, -1);
             if (bestValue < value.first){
                 bestValue = value.first;
                 bestMove = col;
