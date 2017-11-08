32fee4bfd7acc2e7fabea15e2b5a59991e4c3522
dfau <faucin@gmail.com>
2017-11-07 21:42:47 -0300

    me olvide de sacar alfabeta de minimax

--- minimax/main.cpp
+++ minimax/main.cpp
@@ -36,41 +36,6 @@ int minimax(const PossibleMove& node, int depth, bool maximizingPlayer) {
 	}
 }
 
-int alphabeta(const PossibleMove& node, int depth, int alfa,  int beta, bool maximizingPlayer) {
-	if (depth == 0 || node.isTerminal()) {
-		return node.heuristic();
-	}
-
-	if (maximizingPlayer) {
-		int bestValue = std::numeric_limits<int>::min();
-		for (PossibleMove child : node.children()) {
-            // in y out son trampas para poder agregar y sacar fichas en el tablero
-			child.in();
-			int v = alphabeta(child, depth - 1, alfa, beta, false);
-			child.out();
-			bestValue = std::max(bestValue, v);
-			alfa = std::max(alfa,bestValue);
-            if(alfa >= beta) break; //**Beta cut-off**
-        }
-
-        return bestValue;
-    }
-    else {
-    	int bestValue = std::numeric_limits<int>::max();
-    	for (PossibleMove child : node.children()) {
-            // in y out son trampas para poder agregar y sacar fichas en el tablero
-    		child.in();
-    		int v = alphabeta(child, depth - 1, alfa, beta, true);
-    		child.out();
-    		bestValue = std::min(bestValue, v);
-    		beta = std::min(beta, bestValue);
-            if(alfa >= beta) break; //**Alpha cut-off**
-        }
-
-        return bestValue;
-    }
-}
-
 class PlayerMinimax : public Player {
 public:
 	int nextMove(Game& game) {
@@ -101,40 +66,6 @@ public:
 
         std::cerr << game.board().possibleMoves().size() << "; "<< timeFindingMove.count() << "; "<< game.board().columns() <<"; "<< game.board().rows() <<"; " << game.cToWin()<<endl;
         
-        return max->move();
-    }
-};
-
-class PlayerAlfaBeta : public Player {
-public:
-	int nextMove(Game& game) {
-        // TODO traer p del juego
-
-
-		high_resolution_clock::time_point beginMove = high_resolution_clock::now();
-
-        auto moves = PossibleMove(game, -1).children(); // -1 ya que no se usa ese valor
-        
-        auto max = max_element(moves.begin(), moves.end(),
-        	[game](const PossibleMove& m1, const PossibleMove& m2) {
-        		m1.in();
-        		auto res1 = alphabeta(m1, game.remainingPieces(), -1, 1, false);
-        		m1.out();
-        		m2.in();
-        		auto res2 = alphabeta(m2, game.remainingPieces(), -1, 1, false);
-        		m2.out();
-        		return res1 < res2;
-        	}
-        	);
-
-        high_resolution_clock::time_point endMove = high_resolution_clock::now();
-        
-        duration<double> timeFindingMove = duration_cast< duration<double> >(endMove - beginMove);
-
-        assert(max != moves.end()); // encontró alguno
-
-        std::cerr << game.board().possibleMoves().size() << "; "<< timeFindingMove.count() << "; "<< game.board().columns() <<"; "<< game.board().rows() << "; " << game.cToWin()<<endl;
-
         return max->move();
     }
 };
